3e29ce4d2ba48c6ddb7d249a099abe698ed8ff08
Ivan Dimitrov <mmo_unstable@abv.bg>
2023-11-11 14:11:05 +0200

    Add abstract methods, exitApp, addEmployee, fireEmployee, listEmployees, editEmployee

--- src/com/sirma/internal/staffmanagementsystem/Manager.java
+++ src/com/sirma/internal/staffmanagementsystem/Manager.java
@@ -1,4 +1,14 @@
 package com.sirma.internal.staffmanagementsystem;
 
+import java.util.List;
+
 public interface Manager {
+
+    void exitApp();
+
+    void addEmployee(Employee employee);
+
+    void editEmployee(int id);
+    void fireEmployee(int id);
+    void listEmployees();
 }
