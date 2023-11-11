e82ba4ccd680d4a5352800e3cfc873a38027d929
Ivan Dimitrov <mmo_unstable@abv.bg>
2023-11-11 14:35:40 +0200

    Add concrete implementation class WriterCSV for CustomWriter interface.

---
+++ src/com/sirma/internal/staffmanagementsystem/WriterCSV.java
@@ -0,0 +1,45 @@
+package com.sirma.internal.staffmanagementsystem;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class WriterCSV implements CustomWriter {
+
+    @Override
+    public void write(String uri, List<Employee> employees) {
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = Files.newBufferedWriter(Path.of(uri));
+            bufferedWriter.write(appendLine(employees));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                bufferedWriter.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+    }
+
+    private String appendLine(List<Employee> employees) {
+        StringBuilder sb = new StringBuilder();
+        for (Employee employee : employees
+        ) {
+            sb.append(employee.getId() + ",");
+            sb.append(employee.getName() + ",");
+            sb.append(employee.getStartDate() + ",");
+            sb.append(employee.getEndDate() + ",");
+            sb.append(employee.getDepartment() + ",");
+            sb.append(employee.getRole() + ",");
+            sb.append(employee.getSalary());
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+}
