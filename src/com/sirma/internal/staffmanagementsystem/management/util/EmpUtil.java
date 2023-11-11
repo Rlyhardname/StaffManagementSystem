89b72af9b72fa82d0711408e1cfd3b042aff49dc
Ivan Dimitrov <mmo_unstable@abv.bg>
2023-11-11 15:51:56 +0200

    Add EmpUtil with parseEntry method moved from WriteSCV

---
+++ src/com/sirma/internal/staffmanagementsystem/management/util/EmpUtil.java
@@ -0,0 +1,34 @@
+package com.sirma.internal.staffmanagementsystem.management.util;
+
+import com.sirma.internal.staffmanagementsystem.management.enums.Department;
+import com.sirma.internal.staffmanagementsystem.management.model.Employee;
+
+import java.time.LocalDate;
+
+public class EmpUtil {
+
+    public static Employee parseEntry(String[] entry) {
+        long id;
+        String name;
+        LocalDate startDate;
+        LocalDate endDate;
+        Department department;
+        String role;
+        double salary;
+        try {
+            id = Long.parseLong(entry[0]);
+            name = entry[1];
+            String[] dateOne = entry[2].split("/");
+            startDate = LocalDate.of(Integer.parseInt(dateOne[0]), Integer.parseInt(dateOne[1]), Integer.parseInt(dateOne[2]));
+            String[] dateTwo = entry[3].split("/");
+            endDate = LocalDate.of(Integer.parseInt(dateTwo[0]), Integer.parseInt(dateTwo[1]), Integer.parseInt(dateTwo[2]));
+            department = null;
+            role = entry[5];
+            salary = Double.parseDouble(entry[6]);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+
+        return new com.sirma.internal.staffmanagementsystem.management.model.Employee(id, name, startDate, endDate, department, role, salary);
+    }
+}
