762191e341bede04a51e9af0fff62691381479b0
Ivan Dimitrov <mmo_unstable@abv.bg>
2023-11-11 14:09:58 +0200

    Changed signature to take employee list.

--- src/com/sirma/internal/staffmanagementsystem/CustomWriter.java
+++ src/com/sirma/internal/staffmanagementsystem/CustomWriter.java
@@ -1,5 +1,7 @@
 package com.sirma.internal.staffmanagementsystem;
 
+import java.util.List;
+
 public interface CustomWriter {
-    void write(String uri);
+    void write(String uri, List<Employee> employees);
 }
