2d6b61ba57f7b0ad528bd6ff6bbc89dba8b7cceb
Ivan Dimitrov <mmo_unstable@abv.bg>
2023-11-11 11:14:54 +0200

    Add Class Employee with constructor and setters/getters

---
+++ src/com/sirma/internal/staffmanagementsystem/Employee.java
@@ -0,0 +1,72 @@
+package com.sirma.internal.staffmanagementsystem;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private long Id;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Department department;
+    private String role;
+    private double salary;
+
+    public Employee(String[] fields) {
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
