42c5d28f06084e3532ef05d6f76f2cd2ab57da3a
Ivan Dimitrov <mmo_unstable@abv.bg>
2023-11-11 10:13:54 +0200

    Apply given boilerplate code/Template

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+// implement fileReader/fileWriter to handle saving into csv/json
+//        Service service = new StaffService(reader, writer);
+//        Manager manager = new StaffManager(service);
+//        System.out.println("Welcome to Staff Management System");
+//        displayCommands();
+//        boolean isRunning = true;
+//        while (isRunning) {
+//            manager.execute(command);
+// Add Employee
+// 1, Peter Peterson, IT, Junior Java Developer, 1400.50
+// Add Employee
+// 2, Ivan Ivanson, IT, Junior Front-End Developer, 1400.00
+// Edit 1
+// 1, Peter Peterson, IT, Java Developer, 2500.00
+// List employees
+// Search Department Marketing
+// Search Id 1
+// Fire 1
+// Search Name Peter
+// Save &amp; Exit
     }
 }
